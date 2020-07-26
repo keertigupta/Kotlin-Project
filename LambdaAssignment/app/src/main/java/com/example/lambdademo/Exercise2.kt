b948f2d7a0c3e0909eb78243fc60868630f31093
keerti gupta <keerti.gupta@credr.com>
2020-07-26 13:34:00 +0530

    Add Exercise2 without println in when

--- LambdaAssignment/app/src/main/java/com/example/lambdademo/Exercise2.kt
+++ LambdaAssignment/app/src/main/java/com/example/lambdademo/Exercise2.kt
@@ -8,11 +8,12 @@ fun main(){
 
     val grade = {totalnumber:Int->
         when(totalnumber){
-            in 0..35 ->println("fail")
-            in 36..60 ->println("Passed")
-            in 61..100->println("Passed with distinction")
+            in 0..35 ->"fail"
+            in 36..60 ->"Passed"
+            in 61..100->"Passed with distinction"
+            else ->"Less than 0 or more than 100"
         }
     }
-    grade(40)
+        println("Grade is :- ${grade(40)}")
 
 }
