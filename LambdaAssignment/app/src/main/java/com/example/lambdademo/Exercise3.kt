47e27542722cf21ae5ee91e42d6cd634313f6eb4
keertigupta <keerti.373@gmail.com>
2020-07-26 13:48:54 +0530

    Add Exercise3 for Higher Order function

--- LambdaAssignment/app/src/main/java/com/example/lambdademo/Exercise3.kt
+++ LambdaAssignment/app/src/main/java/com/example/lambdademo/Exercise3.kt
@@ -6,4 +6,11 @@ package com.example.lambdademo
 
 fun main(){
 
+    val sum = {a:Int,b:Int,c:Int->
+                                    a+b+c }
+    val average = {firstNo:Int,secondNo:Int,someFunc:(Int,Int,Int)->Int ->
+        ( firstNo+secondNo+someFunc(2,2,2))/3
+    }
+
+    println("Average is ${average(3,3,sum)}")
 }
